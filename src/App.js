e0596cab0c0f5df45ce1e855d9cd8f6d298001d5
danielsky <karma0rg4u@gmail.com>
2019-10-21 22:43:06 +0100

    Update app with shop page components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
-import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-)
+import ShopPage from './pages/shop/shop.component.jsx';
+import './App.css';
 
 function App() {
   return (
     <div>
         <Route exact path='/' component={HomePage}></Route>
-        <Route exact path='/hats' component={HatsPage}></Route>
+        <Route exact path='/shop' component={ShopPage}></Route>
     </div>
   );
 }
